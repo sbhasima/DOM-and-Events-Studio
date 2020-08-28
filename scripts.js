616e3d9c85972c0ff21c72df4e13dbb7a1491096
surbbhas4 <surbbhas4@gmail.com>
2020-08-27 22:22:21 -0400

    add bonus mission. the rocket comback to original position when clicked on abort or landed

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function init(){
     let status = document.getElementById('flightStatus');
     let backgroundColor = document.getElementById('shuttleBackground');
     let shuttleHeight = document.getElementById('spaceShuttleHeight')
+    debugger
 
     rocket = document.getElementById("rocket");
     rocket.style.position = "relative";
@@ -26,43 +27,71 @@ function init(){
         status.innerHTML = 'The shuttle has landed.';
         backgroundColor.style.background = 'green';
         shuttleHeight.innerHTML = '0';
+        rocket.style.left = "0px";
+        rocket.style.bottom = "0px"
     })
 
     let abortMission = document.getElementById("missionAbort")
     abortMission.addEventListener("click", function(){
         result = window.confirm("Confirm that you want to abort the mission.");
         if (result){
+            debugger
             status.innerHTML = "Mission aborted";
             backgroundColor.style.background = "green";
             shuttleHeight.innerHTML = '0';
+            rocket.style.left = "0px";
+            rocket.style.bottom = "0px"
         }
     })
     
     
     let upBtn = this.document.getElementById('upBtn');
     upBtn.addEventListener("click", function(){
-        move = parseInt(rocket.style.bottom)+10+'px';
+        move = parseInt(rocket.style.bottom);
+        if(move<=40){
+            move = move+10+'px';
         rocket.style.bottom = move;
         shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML)+10000;
         console.log(move)
+        }else{
+            alert("Rocket cannot move further")
+        }
+        
     })
     let downBtn = this.document.getElementById('downBtn');
     downBtn.addEventListener("click", function(){
-        move = parseInt(rocket.style.bottom) - 10+'px';
+        move = parseInt(rocket.style.bottom);
+        if(move>-260){
+            move = move - 10+'px';
         rocket.style.bottom = move;
         shuttleHeight.innerHTML = parseInt(shuttleHeight.innerHTML) - 10000;
+        } else{
+            alert("Rocket cannot move further.")
+        }
+        
     })
     let rightBtn = this.document.getElementById('rightBtn');
     rightBtn.addEventListener("click", function(){
-        move = parseInt(rocket.style.left)+10+'px';
-        rocket.style.left = move;
-        console.log(move)
+        move = parseInt(rocket.style.left)
+        if(move < 300){
+            move = move +10+'px';
+            rocket.style.left = move;
+            console.log(move)
+        }else{
+            alert("Rocket cannot move further.")
+        }
+        
     })
     let leftBtn = this.document.getElementById('leftBtn');
     leftBtn.addEventListener("click", function(){
-        move = parseInt(rocket.style.left) - 10+'px';
-        rocket.style.left = move;
-        console.log(move)
+        move = parseInt(rocket.style.left)
+        if(move > -300){
+            move = move - 10+'px';
+            rocket.style.left = move;
+            console.log(move)
+        } else{
+            alert("Rocket cannot move further.")
+        }
     })
     
 
